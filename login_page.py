2cacf364b49044d957e41da5d915036f95a96679
ferris <ferrissava14@gmail.com>
2025-07-26 19:43:05 +0700

    label product

--- login_page.py
+++ login_page.py
@@ -66,11 +66,81 @@ class loginPage:
         tk.Label(self.root, text= f"selamat datang, {current_user}", font=("Helfetica",16)).pack(pady=20)
         tk.Button(self.root, text= "log out", command= self.logout).pack(pady=20)
         tk.Button(self.root, text="Profil", command= self.show_profil).pack(pady=10) 
+        tk.Button(self.root, text="liht produk", command= self.show_product_list).pack(pady=10)
         
     def show_dasboard_admin(self):
         self.clear_window()
         tk.Label(self.root, text= f"admin dasboard, {current_user}", font=("Helfetica",16)).pack(pady=20)
         tk.Button(self.root, text= "log out", command= self.logout).pack(pady=20)
+
+        tk.Button(self.root, text= "tambah produk", command= self.show_add_product).pack(pady=10)
+
+
+    def show_add_product(self) :
+        self.clear_window()
+        tk.Label(self.root, text="tambah produk", font=("Helfetica",16)).pack(pady=10)
+
+        form = tk.Frame(self.root)
+
+        form.pack(pady=10)
+        tk.Label(form, text="nama produk",font=("Helfetica",16 )).grid(row=0, column=0, sticky="e", padx=5, pady=5)
+        self.product_name = tk.Entry(form)
+        self.product_name.grid(row=0, column=1, padx=5, pady=5)
+
+        tk.Label(form, text="harga",font=("Helfetica",16 )).grid(row=1, column=0, sticky="e", padx=5, pady=5)
+        self.product_price = tk.Entry(form)
+        self.product_price.grid(row=1, column=1, padx=5, pady=5)
+        
+        tk.Label(form, text="deskripsi",font=("Helfetica",16 )).grid(row=2, column=0, sticky="e", padx=5, pady=5)
+        self.product_description = tk.Entry(form)
+        self.product_description.grid(row=2, column=1, padx=5, pady=5)
+        
+        tk.Button(self.root, text="simpan produk", command= self.save_produk).pack(pady=10)
+        tk.Button(self.root, text="kembali", command= self.show_dasboard_admin).pack
+
+    
+    def show_product_list(self):
+        self.clear_window()
+        tk.Label(self.root, text="daftar produk", font=("Helfetica",16)).pack(pady=10)
+
+        cursor.execute("SELECT name, price, description FROM products")
+        producks = cursor.fetchall()
+
+        if not producks:
+            tk.Label(self.root, text="belom ada produck").pack(pady=10)
+        else:
+            for name,price,desc in producks:
+                frame = tk.Frame(self.root, borderwidth=1, relief="solid", padx= 10, pady=5)
+                frame.pack(pady=5, padx=10, fill="x")
+
+                tk.Label(frame, text=f"nama: {name}", font=("Helfetica",12,"bold")).pack(anchor="w")
+                tk.Label(frame, text=f"harga: Rp {price:,.0f}").pack(anchor="w")
+                tk.Label(frame, text=f"deskripsi {desc}").pack(anchor="w")
+            
+        tk.Button(self.root, text="kembali", command=self.show_dasboard_user).pack(pady=10)
+        
+
+
+    def save_produk(self):
+        name = self.product_name.get()
+        price = self.product_price.get()
+        description = self.product_description.get()
+
+        if not name or not price :
+            messagebox.showerror("error","nama dan harga produk harus di isi")
+            return
+        
+        try :
+            price = float(price)
+        except ValueError:
+            messagebox.showerror("error","harga harus berupa angka")
+            return
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO products (name, price, description) VALUES (%s, %s, %s)", (name, price, description))
+        db.commit()
+        messagebox.showinfo("sukses","produk berhasil di tambah")
+        self.show_dasboard_admin()
+        
         
 
     def register(self):
@@ -80,9 +150,6 @@ class loginPage:
         cursor = db.cursor()
         cursor.execute("SELECT username FROM users WHERE username = %s",(username,))
         result = cursor.fetchone()
-
-        print(username)
-        print(password)
     
         if result:
             messagebox.showerror("error", "username sudah di pakai")
@@ -99,6 +166,7 @@ class loginPage:
     def login(self):
         username = self.username_login.get()
         password = self.password_login.get()
+
 
         global current_user
         db= database
